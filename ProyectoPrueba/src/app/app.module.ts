e7ee211a03873e63d9bfddb5e44e7e4a0d917f34
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2022-11-14 16:25:34 +0100

    componente series

--- ProyectoPrueba/src/app/app.module.ts
+++ ProyectoPrueba/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { MiComponente } from './components/mi-componente/mi-componente.component';
 import { PeliculasComponent } from './components/peliculas/peliculas.component';
 import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { SeriesComponent } from './components/series/series.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MiComponente,
     PeliculasComponent,
-    PruebasComponent
+    PruebasComponent,
+    SeriesComponent
   ],
   imports: [
     BrowserModule
--- ProyectoPrueba/src/app/components/peliculas/peliculas.component.ts
+++ ProyectoPrueba/src/app/components/peliculas/peliculas.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+//clase OnInit para usar el hook OnInit
 @Component({
   selector: 'app-peliculas',
   templateUrl: './peliculas.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log("Constructor lanzado");
+  }
 
+  /* Se ejecuta cuando cargamos la directiva de nuestro componente,
+  es el primer método que se ejecuta después de lanzar el constructor de la clase del componente.*/
   ngOnInit(): void {
+    console.log("componente lanzado")
   }
 
 }
