649d9d42cfc00c1434d71ea90b7d13735ac32f8e
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2022-11-14 16:39:30 +0100

    comentarios random sobre hook y constructor

--- ProyectoPrueba/src/app/components/peliculas/peliculas.component.ts
+++ ProyectoPrueba/src/app/components/peliculas/peliculas.component.ts
@@ -7,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
+  /**
+   * El constructor asigna un valor a diferentes propiedaddes de la clase
+   * (NO METER LOGICA DENTRO)
+   * no recoger parametros en plan url o cualquier cosa , es mala practica
+   * - Tambien podemos precargar cosas que sean necesarias a través del constructor
+   */
   constructor() {
     console.log("Constructor lanzado");
   }
 
-  /* Se ejecuta cuando cargamos la directiva de nuestro componente,
-  es el primer método que se ejecuta después de lanzar el constructor de la clase del componente.*/
+  /**
+   * Se ejecuta cuando cargamos la directiva de nuestro componente,
+   * es el primer método que se ejecuta después de lanzar el constructor de la clase 
+   * del componente.
+   */
+  /**
+   * podemos meter logica dentro 
+   * 
+   */
   ngOnInit(): void {
     console.log("componente lanzado")
   }
